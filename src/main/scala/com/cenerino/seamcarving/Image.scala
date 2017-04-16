60b0a6e2c5329e0f7a0177870bdc9daf5d1f1c1f
Marcelo Cenerino <marcelocenerine@gmail.com>
2017-04-16 19:37:03 +0100

    revert to 2D array due to performance issues

--- src/main/scala/com/cenerino/seamcarving/Image.scala
+++ src/main/scala/com/cenerino/seamcarving/Image.scala
@@ -1,16 +1,15 @@
 package com.cenerino.seamcarving
 
-import Image._
 import java.awt.image.BufferedImage
 import java.awt.image.BufferedImage.TYPE_INT_RGB
 import java.io.{File, FileNotFoundException}
 import javax.imageio.ImageIO
 
-class Image private(private val pixels: Array[RGB], val width: Int, val height: Int) extends PartialFunction[Pos, RGB]{
+class Image private(private val pixels: Array[Array[RGB]], val width: Int, val height: Int) extends PartialFunction[Pos, RGB]{
 
   def apply(pos: Pos): RGB = {
     require(isDefinedAt(pos), "Invalid coordinates")
-    pixels(to1DimIndex(pos, width))
+    pixels(pos._1)(pos._2)
   }
 
   override def isDefinedAt(pos: Pos): Boolean = {
@@ -18,24 +17,17 @@ class Image private(private val pixels: Array[RGB], val width: Int, val height: 
     col >= 0 && col < width && row >= 0 && row < height
   }
 
+  // TODO remove?
   def update(pos: Pos, rgb: RGB): Unit = {
     require(isDefinedAt(pos), "Invalid coordinates")
-    pixels(to1DimIndex(pos, width)) = rgb
+    pixels(pos._1)(pos._2) = rgb
   }
 
   def transpose: Image = {
-    val transposedPixels = Array.ofDim[RGB](width * height)
-
-    for {
-      row <- 0 until height
-      col <- 0 until width
-      sourcePx = to1DimIndex((col, row), width)
-      targetPx = to1DimIndex((row, col), height)
-    } transposedPixels(targetPx) = pixels(sourcePx)
-
-    new Image(transposedPixels, height, width)
+    new Image(pixels.transpose, height, width)
   }
 
+  // FIXME
   override def equals(that: Any): Boolean = that match {
     case other: Image => this.pixels sameElements other.pixels
     case _ => false
@@ -48,10 +40,7 @@ object Image {
     load(path) match {
       case Some(img) => {
         val (width, height) = (img.getWidth, img.getHeight)
-        val pixels = Array.tabulate[RGB](width * height)(i => {
-          val (c, r) = to2DimIndex(i, width)
-          img.getRGB(c, r)
-        })
+        val pixels = Array.tabulate[RGB](width, height)(img.getRGB(_, _))
         new Image(pixels, width, height)
       }
       case None => throw new FileNotFoundException
@@ -66,10 +55,7 @@ object Image {
     }.map(ImageIO.read)
   }
 
-  def blank(width: Int, height: Int, color: RGB = White): Image = new Image(Array.fill(color)(width * height), width, height)
-
-  private def to2DimIndex(index: Int, width: Int): Pos = (index % width, index / width)
-  private def to1DimIndex(pos: Pos, width: Int): Int = pos._1 + (pos._2 * width)
+  def blank(width: Int, height: Int, color: RGB = White): Image = new Image(Array.ofDim(width, height), width, height)
 
   implicit def image2BufferedImage(image: Image): BufferedImage = {
     val bufferedImage = new BufferedImage(image.width, image.height, TYPE_INT_RGB)
